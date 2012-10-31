8024dd85ea82542f666e51db0e338aa436dab4ad
stephwen <s.swenric@gmail.com>
2012-10-31 10:24:52 +0100

    starting to package things with dzil

---
+++ WWW-Google-Movies/lib/example1.pl
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+use WWW::Google::Movies;
+binmode STDOUT, ":encoding(UTF-8)";
+
+my $foo = WWW::Google::Movies->new("city" => "Liège");
+#$foo = WWW::Google::Movies->new("city" => "Liège", "date" => "2012-10-27");
+
+print $foo->get_date."\n";
+print $foo->city."\n";
+print $foo->nbDays."\n";
+print $foo->url."\n";
+
+foreach my $theater (@{$foo->theaters}) {
+	print $theater->name."\n";
+	foreach my $movie (@{$theater->movies}) {
+		print "\t".$movie->name."\n";
+		print "\t".$movie->info."\n";
+		foreach my $time (@{$movie->times}) {
+			print "\t".$time;
+		}
+		print "\n";
+	}
+}
